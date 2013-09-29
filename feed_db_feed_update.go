22a21687439d01a71d5f4df53066b7ccba7a37aa
Matthew Dawson <matthew@mjdsystems.ca>
2013-09-29 03:37:21 -0400

    Start updating the database with feed information.
    The database now has some feed information inserted/updated.  It doesn't
    deal with filled feeds, or deleting, or updating items with new pub dates
    (well, actually it does but just incorrectly).

---
+++ feed_db_feed_update.go
@@ -0,0 +1,176 @@
+/*
+ * Copyright (C) 2013 Matthew Dawson <matthew@mjdsystems.ca>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package main
+
+import (
+	"errors"
+
+	"net/url"
+
+	"sort"
+
+	riak "github.com/tpjg/goriakpbc"
+)
+
+var FeedNotFound = errors.New("Failed to find feed in riak!")
+
+const (
+	MaximumFeedItems = 10000
+)
+
+func drainErrorChannelIntoSlice(errCh <-chan error, errorSlice *[]error, responses int) {
+	for i := 0; i < responses; i++ {
+		err := <-errCh
+		if err != nil {
+			*errorSlice = append(*errorSlice, err)
+		}
+	}
+}
+
+func InsertItem(con *riak.Client, itemKey ItemKey, item ParsedFeedItem) error {
+	itemModel := FeedItem{
+		Title:   item.Title,
+		Author:  item.Author,
+		Content: item.Content,
+		Url:     item.Url,
+		PubDate: item.PubDate,
+	}
+	if err := con.NewModel(string(itemKey), &itemModel); err != nil {
+		return err
+	} else if err = itemModel.Save(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateItem(con *riak.Client, itemKey ItemKey, item ParsedFeedItem, itemModel *FeedItem) error {
+	itemModel.Title = item.Title
+	itemModel.Author = item.Author
+	itemModel.Content = item.Content
+	itemModel.Url = item.Url
+	itemModel.PubDate = item.PubDate
+
+	if err := itemModel.Save(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateFeed(con *riak.Client, feedUrl url.URL, feedData ParsedFeedData, ids <-chan uint64) error {
+	feed := &Feed{Url: feedUrl}
+	if err := con.LoadModel(feed.UrlKey(), feed); err == riak.NotFound {
+		return FeedNotFound
+	} else if err != nil {
+		return err
+	}
+	// First finish clean off all deleted items, and clean out inserted item keys.  Since either existing
+	// means a previous operation has been left in a bad state.  TBI
+
+	// Next update the basic attributes (title basically)
+	feed.Title = feedData.Title
+
+	/* Next find all the feed items to insert/update.  If the item doesn't exist, create it's id and
+	 * mark for insert.  Otherwise mark it for an read/update/store pass.  Make sure to mark for
+	 * deletion items as necessary.
+	 */
+	// This struct holds an ItemKey and a ParsedFeedItem for later parsing.
+	type ToProcess struct {
+		ItemKey ItemKey
+		Data    ParsedFeedItem
+		Model   *FeedItem
+	}
+	NewItems := make([]ToProcess, 0)
+	UpdatedItems := make([]ToProcess, 0)
+
+	for _, rawItem := range feedData.Items {
+		// Try to find the raw Item in the Item Keys list.
+		index := feed.ItemKeys.FindRawItemId(rawItem.GenericKey)
+		if index != -1 {
+			// Found it!  Assume it is an update.
+			p := ToProcess{
+				ItemKey: feed.ItemKeys[index],
+				Data:    rawItem,
+				Model:   &FeedItem{},
+			}
+
+			if err := con.LoadModel(string(p.ItemKey), p.Model); err != nil {
+				return err
+			}
+
+			UpdatedItems = append(UpdatedItems, p)
+		} else {
+			// Nope, lets insert it!
+			NewItems = append(NewItems, ToProcess{
+				Data: rawItem,
+			})
+		}
+	}
+
+	/* Alright, any new items are mentioned in the Feed before being inserted.  In case something
+	 * happens, I'd prefer not to lose an item.  Note the order is reversed so that the oldest story
+	 * will get the smallest id, preserving sort order.  Inserted Item Keys needs to be sorted (well,
+	 * reversed) after this so it is in correct order as well.  This loop violates ItemKeys sort
+	 * order, so the sort is necessary for now. */
+	for i := len(NewItems) - 1; i >= 0; i-- {
+		newItem := &NewItems[i]
+		newItem.ItemKey = NewItemKey(<-ids, newItem.Data.GenericKey)
+		feed.InsertedItemKeys = append(feed.InsertedItemKeys, newItem.ItemKey)
+	}
+	sort.Sort(feed.InsertedItemKeys)
+
+	// Ok, we must save here.  Otherwise planned changes may occur that will not be cleaned up!
+	if err := feed.Save(); err != nil {
+		return err
+	}
+
+	errCh := make(chan error) // All of the errors go into here, to be pulled out.
+
+	// Good, now implement the change and update the Feed.
+
+	// First add new items
+	for _, newItem := range NewItems {
+		feed.ItemKeys = append(feed.ItemKeys, newItem.ItemKey)
+		go func(newItem ToProcess) {
+			errCh <- InsertItem(con, newItem.ItemKey, newItem.Data)
+		}(newItem)
+	}
+	feed.InsertedItemKeys = nil
+
+	// Now update them.
+	for _, newItem := range UpdatedItems {
+		go func(newItem ToProcess) {
+			errCh <- UpdateItem(con, newItem.ItemKey, newItem.Data, newItem.Model)
+		}(newItem)
+	}
+
+	sort.Sort(sort.Reverse(feed.ItemKeys)) // Just sort this.  TBD: Actually maintain this sort order to avoid this!
+
+	//Now, collect the errors
+	var errs []error
+	drainErrorChannelIntoSlice(errCh, &errs, len(NewItems))
+	drainErrorChannelIntoSlice(errCh, &errs, len(UpdatedItems))
+	if len(errs) != 0 {
+		return MultiError(errs)
+	}
+
+	if err := feed.Save(); err != nil {
+		return err
+	}
+
+	_, _ = NewItems, UpdatedItems
+	return nil
+}
---
+++ feed_db_feed_update_test.go
@@ -0,0 +1,186 @@
+/*
+ * Copyright (C) 2013 Matthew Dawson <matthew@mjdsystems.ca>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package main
+
+import (
+	"net/url"
+	"sort"
+
+	riak "github.com/tpjg/goriakpbc"
+
+	"testing"
+)
+
+func makeIdGenerator() <-chan uint64 {
+	ch := make(chan uint64)
+
+	go func(ch chan<- uint64) {
+		var id uint64
+		for {
+			ch <- id
+			id++
+		}
+	}(ch)
+
+	return ch
+}
+
+var testIdGenerator <-chan uint64 = makeIdGenerator()
+
+func fixFeedForMerging(toFix *ParsedFeedData) *ParsedFeedData {
+	sort.Sort(toFix.Items)
+
+	for i, _ := range toFix.Items {
+		item := &toFix.Items[i]
+		item.GenericKey = makeHash(string(item.GenericKey))
+	}
+	return toFix
+}
+
+func compareParsedToFinalFeed(t *testing.T, data *ParsedFeedData, model *Feed, con *riak.Client) bool {
+	// Compare basics:
+	if data.Title != model.Title {
+		t.Error("Feed title didn't match %s vs %s!", data.Title, model.Title)
+		return false
+	}
+
+	if len(data.Items) != len(model.ItemKeys) {
+		if len(data.Items) > MaximumFeedItems {
+			t.Errorf("Item count differs due to items count greater then Maximum number of feed items (%v of %v)", len(data.Items), MaximumFeedItems)
+		} else {
+			t.Errorf("Item count is different %v vs %v!", len(data.Items), len(model.ItemKeys))
+		}
+		return false
+	}
+	if len(model.InsertedItemKeys) != 0 || len(model.DeletedItemKeys) != 0 {
+		t.Error("There are left over inserted or deleted item keys!")
+		return false
+	}
+
+	//Compare saved feed items.  This means a trip through riak!  The order should match though ...
+	for i, itemKey := range model.ItemKeys {
+		modelItem := FeedItem{}
+		if err := con.LoadModel(string(itemKey), &modelItem); err != nil {
+			t.Errorf("Failed to load item! Error: %s", err)
+			return false
+		}
+
+		dataItem := data.Items[i]
+
+		if dataItem.Title != modelItem.Title ||
+			dataItem.Author != modelItem.Author ||
+			dataItem.Content != modelItem.Content ||
+			dataItem.Url != modelItem.Url ||
+			!dataItem.PubDate.Equal(modelItem.PubDate) {
+			t.Errorf("Item data didn't match! Original:\n%#v\nLoaded:\n%#v", dataItem, modelItem)
+			return false
+		}
+	}
+
+	return true
+}
+
+func CreateFeed(t *testing.T, Url *url.URL) Feed {
+	feedModel := &Feed{Url: *Url}
+	if err := con.NewModel(feedModel.UrlKey(), feedModel); err != nil {
+		t.Fatalf("Failed to initialize feed model (%s)!", err)
+	} else if err = feedModel.Save(); err != nil {
+		t.Fatalf("Failed to store feed model (%s)!", err)
+	}
+	return *feedModel
+}
+
+func MustUpdateFeedTo(t *testing.T, con *riak.Client, url *url.URL, feedName string, updateCount int) (feed *ParsedFeedData) {
+	for i := 0; i < updateCount; i++ {
+		feed = getFeedDataFor(t, feedName, i)
+		fixFeedForMerging(feed)
+
+		if err := updateFeed(con, *url, *feed, testIdGenerator); err != nil {
+			t.Errorf("Failed to update simple single feed (%s)!", err)
+		}
+	}
+
+	return
+}
+
+func TestSingleFeedInsert(t *testing.T) {
+	con := getTestConnection(t)
+	defer killTestDb(con, t)
+
+	feed := getFeedDataFor(t, "simple", 0)
+	fixFeedForMerging(feed)
+
+	url, _ := url.Parse("http://example.com/rss")
+
+	err := updateFeed(con, *url, *feed, testIdGenerator)
+	if err != FeedNotFound {
+		t.Fatalf("Failed to insert simple single feed (%s)!", err)
+	}
+
+	feedModel := CreateFeed(t, url)
+
+	err = updateFeed(con, *url, *feed, testIdGenerator)
+	if err != nil {
+		t.Errorf("Failed to update simple single feed (%s)!", err)
+	}
+
+	// Finally, load the feed again and verify properties!
+	loadFeed := &Feed{}
+	if err = con.LoadModel(feedModel.UrlKey(), loadFeed); err != nil {
+		t.Fatalf("Failed to initialize feed model (%s)!", err)
+	} else if compareParsedToFinalFeed(t, feed, loadFeed, con) != true {
+		t.Errorf("Saved feed does not match what was inserted! Original:\n%+v\nLoaded:\n%+v", feed, loadFeed)
+	}
+}
+
+func TestSingleFeedUpdate(t *testing.T) {
+	con := getTestConnection(t)
+	defer killTestDb(con, t)
+
+	url, _ := url.Parse("http://example.com/rss")
+
+	feedModel := CreateFeed(t, url)
+
+	feed := MustUpdateFeedTo(t, con, url, "simple", 2)
+
+	// Finally, load the feed again and verify properties!
+	loadFeed := &Feed{}
+	if err := con.LoadModel(feedModel.UrlKey(), loadFeed); err != nil {
+		t.Fatalf("Failed to initialize feed model (%s)!", err)
+	} else if compareParsedToFinalFeed(t, feed, loadFeed, con) != true {
+		t.Errorf("Saved feed does not match what was inserted! Original:\n%+v\nLoaded:\n%+v", feed, loadFeed)
+	}
+}
+
+func TestFeedUpdateAndInsert(t *testing.T) {
+	con := getTestConnection(t)
+	defer killTestDb(con, t)
+
+	url, _ := url.Parse("http://example.com/rss")
+
+	feedModel := CreateFeed(t, url)
+
+	feed := MustUpdateFeedTo(t, con, url, "simple", 3)
+
+	// Finally, load the feed again and verify properties!
+	loadFeed := &Feed{}
+	if err := con.LoadModel(feedModel.UrlKey(), loadFeed); err != nil {
+		t.Fatalf("Failed to initialize feed model (%s)!", err)
+	} else if compareParsedToFinalFeed(t, feed, loadFeed, con) != true {
+		t.Errorf("Saved feed does not match what was inserted! Original:\n%+v\nLoaded:\n%+v", feed, loadFeed)
+	}
+}
